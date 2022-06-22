97a04f8951070c74f3eb766de91eecdddcbb5173
Danny Moghnie <danny.moghnie@gmail.com>
2022-06-22 16:24:32 -0700

    ipv4_from_ipv4

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -283,10 +283,10 @@ impl NetworkManager {
 
                 let v = ip::v4::Builder::default().id(0x42)?.dscp(dscp)?.ecn(ecn)?
                 .source(src)?.destination(dst)?
-                .ttl(ipv6_pkt.hop_limit())?.protocol(Protocol::Tcp)?.payload(ipv6_pkt.payload())?.build();
-                // .tcp()?.acknowledgment(tcp.acknowledgment())?.destination(tcp.destination())?
-                // .flags(tcp.flags())?.sequence(tcp.sequence())?.source(tcp.source())?
-                // .window(tcp.window())?.pointer(tcp.pointer())?.payload(tcp.payload())?.build();
+                .ttl(ipv6_pkt.hop_limit())?
+                .tcp()?.acknowledgment(tcp.acknowledgment())?.destination(tcp.destination())?
+                .flags(tcp.flags())?.sequence(tcp.sequence())?.source(tcp.source())?
+                .window(tcp.window())?.pointer(tcp.pointer())?.payload(tcp.payload())?.build();
                 log::trace!("ipv4_from_ipv6 - result : ipv6 {:?}", v);
                 return v;
                 
