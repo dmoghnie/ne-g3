bb4cb8d0eccda9a23e5fb28ce10d31e5f56288d0
Danny Moghnie <danny.moghnie@gmail.com>
2022-06-28 11:03:15 -0700

    send_async

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -571,7 +571,7 @@ impl NetworkManager {
                                                         log::trace!("Sending ipv6 packet to G3 {:?}", ipv6_pkt);
                                                         let data_request = AdpDataRequest::new(rand::thread_rng().gen(), &ipv6_pkt, true, 0);
                                                         // sleep(Duration::from_millis(100)).await;
-                                                        self.cmd_tx.send_async(usi::Message::UsiOut(data_request.into())).await;
+                                                        self.cmd_tx.send(usi::Message::UsiOut(data_request.into()));
                                                     },
                                                     Err(e) => {
                                                         log::warn!("Failed to convert ipv6 packet to ipv4 packet : {}", e);
