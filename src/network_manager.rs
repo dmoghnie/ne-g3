b5376997901fd24caa91340dd3e951dd25425e36
Danny Moghnie <danny.moghnie@gmail.com>
2022-06-28 02:25:41 -0700

    no options!

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -404,7 +404,7 @@ impl NetworkManager {
                 .tcp()?.source(tcp.source())?.destination(tcp.destination())?
                 // .window(50000)?
                 .sequence(tcp.sequence())?.acknowledgment(tcp.acknowledgment())?
-                .pointer(tcp.pointer())?.flags(tcp.flags())?.options(tcp.raw_options())?.payload(tcp.payload())?.build();
+                .pointer(tcp.pointer())?.flags(tcp.flags())?.payload(tcp.payload())?.build();
                 // let v = ip::v4::Builder::default().id(0x42)?.dscp(dscp)?.ecn(ecn)?
                 // .source(src)?.destination(dst)?
                 // .ttl(ipv6_pkt.hop_limit())?.tcp()?.payload(tcp)?.build();
