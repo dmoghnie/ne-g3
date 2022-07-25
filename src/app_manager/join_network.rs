9466e66f616a8324059459a7ba4b6eb741946c12
Danny Moghnie <danny.moghnie@gmail.com>
2022-07-25 16:50:12 -0700

    don't set short address

--- src/app_manager/join_network.rs
+++ src/app_manager/join_network.rs
@@ -44,8 +44,8 @@ impl Stateful<State, usi::Message, flume::Sender<usi::Message>, Context> for Joi
                     adp::Message::AdpG3NetworkJoinResponse(response) => {
                         if (response.status == EAdpStatus::G3_SUCCESS) {
                             let v = response.network_addr.to_le_bytes().to_vec();
-                            let request = request::AdpMacSetRequest::new(EMacWrpPibAttribute::MAC_WRP_PIB_SHORT_ADDRESS, 0, &v);
-                            cs.send(usi::Message::UsiOut(request.into()));
+                            // let request = request::AdpMacSetRequest::new(EMacWrpPibAttribute::MAC_WRP_PIB_SHORT_ADDRESS, 0, &v);
+                            // cs.send(usi::Message::UsiOut(request.into()));
                             return Response::Transition(State::Idle);
                         }
                     }
