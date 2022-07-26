68d4d8b8e0a3409176e0a553a5c862c8e6085a74
Danny Moghnie <danny.moghnie@gmail.com>
2022-07-25 21:34:44 -0700

    different parameters

--- src/app_config.rs
+++ src/app_config.rs
@@ -28,8 +28,14 @@ pub const RAND_S_DEFAULT: [u8; 16] = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x
 pub const CONF_CONTEXT_INFORMATION_TABLE_0: [u8; 14] = [
     0x2, 0x0, 0x1, 0x50, 0xfe, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x78, 0x1d,
 ];
-pub const CONF_CONTEXT_INFORMATION_TABLE_1: [u8; 10] =
-    [0x2, 0x0, 0x1, 0x30, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66];
+// pub const CONF_CONTEXT_INFORMATION_TABLE_1: [u8; 10] =
+//     [0x2, 0x0, 0x1, 0x30, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66];
+
+pub const CONF_CONTEXT_INFORMATION_TABLE_1: [u8; 12] = [
+    0x2, 0x0, 0x1, 0x40, 0xfd, 0x00, 0x0, 0x0, 0x0, 0x2, 0x78, 0x1d,
+];
+
+// 0xfd, 0x00, 0x00, 0x00, 0x00, 0x02, 0x78, 0x1d
 
 pub const X_IDS_ARIB: [u8; 34] = [0x53, 0x4D, 0xAD, 0xB2, 0xC4, 0xD5, 0xE6, 0xFA, 0x53, 0x4D, 0xAD, 0xB2, 0xC4, 0xD5, 0xE6, 0xFA,
 0x53, 0x4D, 0xAD, 0xB2, 0xC4, 0xD5, 0xE6, 0xFA, 0x53, 0x4D, 0xAD, 0xB2, 0xC4, 0xD5, 0xE6, 0xFA,
@@ -128,6 +134,9 @@ lazy_static! {
                 vec![0x10, 0x00]
             ),
             // (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DEFAULT_COORD_ROUTE_ENABLED.into(), 0, vec![0x01]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_ROUTING_TABLE_ENTRY_TTL.into(), 0, vec![0xB4, 0x00]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DESTINATION_ADDRESS_SET.into(), 0, vec![0xFF, 0x7F]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DISABLE_DEFAULT_ROUTING.into(), 0, vec![0x1]),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MAX_HOPS.into(), 0, vec![0x0A]),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MANUF_EAP_PRESHARED_KEY.into(), 0, CONF_PSK_KEY.to_vec()),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_CONTEXT_INFORMATION_TABLE.into(), 0, CONF_CONTEXT_INFORMATION_TABLE_0.to_vec()),
@@ -174,6 +183,9 @@ lazy_static! {
                 vec![0x10, 0x00]
             ),
             // (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DEFAULT_COORD_ROUTE_ENABLED.into(), 0, vec![0x01]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_ROUTING_TABLE_ENTRY_TTL.into(), 0, vec![0xB4, 0x00]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DESTINATION_ADDRESS_SET.into(), 0, vec![0xFF, 0x7F]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DISABLE_DEFAULT_ROUTING.into(), 0, vec![0x1]),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MAX_HOPS.into(), 0, vec![0x0A]),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MANUF_EAP_PRESHARED_KEY.into(), 0, CONF_PSK_KEY.to_vec()),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_CONTEXT_INFORMATION_TABLE.into(), 0, CONF_CONTEXT_INFORMATION_TABLE_0.to_vec()),
