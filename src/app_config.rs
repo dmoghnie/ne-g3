ae8ae528694033bd7403942edc53630ede019b4a
Danny Moghnie <danny.moghnie@gmail.com>
2022-07-27 01:27:32 -0700

    secur

--- src/app_config.rs
+++ src/app_config.rs
@@ -123,7 +123,15 @@ lazy_static! {
                 0,
                 PAN_ID.to_be_bytes().to_vec()
             ),
-            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_SECURITY_LEVEL.into(), 0, vec![0x00]),
+            (
+                G3ParamType::Mac,
+                adp::EMacWrpPibAttribute::MAC_WRP_PIB_KEY_TABLE.into(),
+                0,
+                g_au8CurrGMK.to_vec()
+            ),
+            //TODO rekey 
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_SECURITY_LEVEL.into(), 0, vec![0x05]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_ACTIVE_KEY_INDEX.into(), 0, vec![0x00]),
     
             (
                 G3ParamType::Adp,
@@ -135,6 +143,7 @@ lazy_static! {
             // (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_ROUTING_TABLE_ENTRY_TTL.into(), 0, vec![0xB4, 0x00]),
             // (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DESTINATION_ADDRESS_SET.into(), 0, vec![0xFF, 0x7F]),
             // (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_DISABLE_DEFAULT_ROUTING.into(), 0, vec![0x0]),
+            
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MAX_HOPS.into(), 0, vec![0x0A]),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_MANUF_EAP_PRESHARED_KEY.into(), 0, CONF_PSK_KEY.to_vec()),
             (G3ParamType::Adp,adp::EAdpPibAttribute::ADP_IB_CONTEXT_INFORMATION_TABLE.into(), 0, CONF_CONTEXT_INFORMATION_TABLE_0.to_vec()),
@@ -167,7 +176,7 @@ lazy_static! {
                 0,
                 PAN_ID.to_be_bytes().to_vec()
             ),
-            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_SECURITY_LEVEL.into(), 0, vec![0x0]),
+            (G3ParamType::Adp, adp::EAdpPibAttribute::ADP_IB_SECURITY_LEVEL.into(), 0, vec![0x05]),
     
             (
                 G3ParamType::Adp,
